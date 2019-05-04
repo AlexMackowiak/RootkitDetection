47d3777876ce1718c915916335e3c591c9fc1aa0
AlexMackowiak <Alex@Mackowiaks.com>
2019-05-03 22:40:48 -0500

    Initial fork-based detection setup

---
+++ rk_fb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define NUM_CHILDREN 10
+
+int main() {
+	int i;
+	for(i = 0; i < NUM_CHILDREN; i++) {
+		pid_t child_pid = fork();
+
+		if (child_pid == -1) {
+			// Shit went down
+			if (errno == EAGAIN) {
+				// Some other error
+				printf("EAGAIN\n");
+			}
+			if (errno == ENOMEM) {
+				// That's the money
+				printf("ENOMEM\n");
+			} 
+		}
+
+		if (child_pid != 0) {
+			// Parent
+			int status;
+			wait(child_pid, &status, 0);
+			printf("Parent of %d exiting\n", child_pid);
+			break;
+		} else {
+			printf("Child with %d created\n", getpid());
+		}
+	}
+
+	return 0;
+}
