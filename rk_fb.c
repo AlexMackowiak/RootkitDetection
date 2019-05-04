d9ffc44027b396d06cfec9946c57e0b1e2efc229
AlexMackowiak <Alex@Mackowiaks.com>
2019-05-03 22:51:51 -0500

    Added progress indicator

--- rk_fb.c
+++ rk_fb.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define NUM_CHILDREN 10
+#define NUM_CHILDREN 10000
 
 int main() {
 	int i;
@@ -21,14 +21,18 @@ int main() {
 			} 
 		}
 
+		if(i % 1000 == 0) {
+			printf("%d child processes created", i);
+		}
+
 		if (child_pid != 0) {
 			// Parent
 			int status;
 			wait(child_pid, &status, 0);
-			printf("Parent of %d exiting\n", child_pid);
+			//printf("Parent of %d exiting\n", child_pid);
 			break;
 		} else {
-			printf("Child with %d created\n", getpid());
+			//printf("Child with %d created\n", getpid());
 		}
 	}
 
