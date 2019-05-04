8930ec56a38bfa2c4d4370cde75217b99410b7af
AlexMackowiak <Alex@Mackowiaks.com>
2019-05-04 15:24:22 -0500

    Child process are now created in cycles to hopefully cut down on creation time

--- rk_fb_parent.c
+++ rk_fb_parent.c
@@ -5,63 +5,68 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 
-#define NUM_CHILDREN 10000
+#define NUM_CHILDREN_PER_CYCLE 100
+#define NUM_CYCLES 200
+#define NUM_CHILDREN (NUM_CHILDREN_PER_CYCLE * NUM_CYCLES)
 
-volatile sig_atomic_t stopChildren;
-void handleChildSignal(int signal) {
-	stopChildren = 1;
-}
+static int* numPausedInCycle;
 
 int main() {
-	signal(SIGUSR1, handleChildSignal);
+	numPausedInCycle = mmap(NULL, sizeof(*numPausedInCycle), PROT_READ | PROT_WRITE,
+							MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	pid_t* child_pids = (pid_t*) calloc(NUM_CHILDREN, sizeof(pid_t));
-	int is_parent = 0;
-	stopChildren = 0;
-
+	
 	int i;
-	for(i = 0; i < NUM_CHILDREN; i++) {
-		pid_t child_pid = fork();
+	for(i = 0; i < NUM_CYCLES; i++) {
+		int j;
+		for(j = 0; j < NUM_CHILDREN_PER_CYCLE; j++) {
+			int childIndex = (i * NUM_CHILDREN_PER_CYCLE + j);
+			pid_t child_pid = fork();
 
-		if (child_pid == -1) {
-			// Shit went down
-			if (errno == EAGAIN) {
-				// Some other error
-				printf("EAGAIN\n");
-				break;
+			if (child_pid == -1) {
+				// Shit went down
+				if (errno == EAGAIN) {
+					// Some other error
+					printf("EAGAIN\n");
+					break;
+				}
+				if (errno == ENOMEM) {
+					// That's the money
+					printf("ENOMEM\n");
+					break;
+				} 
 			}
-			if (errno == ENOMEM) {
-				// That's the money
-				printf("ENOMEM\n");
-				break;
-			} 
-		}
 
-		if (child_pid != 0) {
-			//printf("Child created with pid: %d\n", child_pid);
-			child_pids[i] = child_pid;
-			is_parent = 1;
-		} else {
-			if(i % 100 == 0) {
-				printf("%d child processes created\n", i);
-			}
-			if(!stopChildren) {
+			if (child_pid != 0) {
+				//printf("Child created with pid: %d\n", child_pid);
+				child_pids[childIndex] = child_pid;
+			} else {
+				//if(j == (NUM_CHILDREN_PER_CYCLE - 1)) {
+					// Last child in this cycle
+					//printf("%d child processes created\n", childIndex);
+				//}
+				//printf("CHILD PAUSING\n");
+				*numPausedInCycle += 1;
 				pause();
+				//printf("Child exiting with pid: %d\n", getpid());
+				exit(0);
 			}
-			//printf("Child exiting with pid: %d\n", getpid());
-			break;
 		}
+
+		while(*numPausedInCycle < NUM_CHILDREN_PER_CYCLE);
+		*numPausedInCycle = 0;
+		printf("%d child processes created and paused\n", (i + 1) * NUM_CHILDREN_PER_CYCLE);
+		//printf("Next cycle\n");
 	}
 
-	if(is_parent) {
-		int status;
-		for(i = 0; i < NUM_CHILDREN; i++) {	
-			//kill(child_pids[i], SIGUSR1);
-			//kill(child_pids[i], SIGINT);
-			waitpid(child_pids[i], &status, 0);
-		}
-		free(child_pids);
+	int status;
+	for(i = 0; i < NUM_CHILDREN; i++) {	
+		waitpid(child_pids[i], &status, 0);
 	}
+	munmap(numPausedInCycle, sizeof(*numPausedInCycle));
+	free(child_pids);
 
 	return 0;
 }
