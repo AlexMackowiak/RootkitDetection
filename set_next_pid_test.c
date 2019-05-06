8ad62453dd09dac0d489de8fdf59db59d69748c4
AlexMackowiak <Alex@Mackowiaks.com>
2019-05-06 19:58:31 +0000

    Creative workaround with setting last pid may solve issues

--- set_next_pid_test.c
+++ set_next_pid_test.c
@@ -3,61 +3,50 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/file.h>
 
+// Code heavily inspired by: 
+// efiop-notes.blogspot.com/2014/06/how-to-set-pid-using-nslast-pid.html
 int main(int argc, char *argv[]) {
-    int fd, pid;
-    char buf[32];
-
     if (argc != 2)
      return 1;
 
-    printf("Opening ns_last_pid...\n");
-    fd = open("/proc/sys/kernel/ns_last_pid", O_RDWR | O_CREAT, 0644);
-    if (fd < 0) {
+    int last_pid_fd = open("/proc/sys/kernel/ns_last_pid", O_RDWR | O_CREAT, 0644);
+    if (last_pid_fd < 0) {
         perror("Can't open ns_last_pid");
         return 1;
     }
-    printf("Done\n");
 
-    printf("Locking ns_last_pid...\n");
-    if (flock(fd, LOCK_EX)) {
-        close(fd);
+    if (flock(last_pid_fd, LOCK_EX)) {
+        close(last_pid_fd);
         printf("Can't lock ns_last_pid\n");
         return 1;
     }
-    printf("Done\n");
 
-    pid = atoi(argv[1]);
-    snprintf(buf, sizeof(buf), "%d", pid - 1);
+    int target_pid = atoi(argv[1]);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "%d", target_pid - 1);
 
-    printf("Writing pid-1 to ns_last_pid...\n");
-    if (write(fd, buf, strlen(buf)) != strlen(buf)) {
-        printf("Can't write to buf\n");
+    if (write(last_pid_fd, buf, strlen(buf)) != strlen(buf)) {
+        printf("Error writing to buf\n");
         return 1;
     }
-    printf("Done\n");
-
-    printf("Forking...\n");
-    int new_pid;
-    new_pid = fork();
-    if (new_pid == 0) {
-        printf("I'm child!\n");
-        exit(0);
-    } else if (new_pid == pid) {
-        printf("I'm parent. My child got right pid!\n");
-    } else {
-        printf("pid does not match expected one\n");
-    }
-    printf("Done\n");
-
-    printf("Cleaning up...");
-    if (flock(fd, LOCK_UN)) {
+    if (flock(last_pid_fd, LOCK_UN)) {
         printf("Can't unlock");
     }
+    close(last_pid_fd);
 
-    close(fd);
+	int i;
+	for(i = 0; i < 10; i++) {
+		int child_pid = fork();
+		if (child_pid == 0) {
+			pause();
+			exit(0);
+		}
+		printf("Child pid: %d\n", child_pid);
+	}
 
-    printf("Done\n");
-
+	pause();
     return 0;
 }
